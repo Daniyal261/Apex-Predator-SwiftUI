16b87551c7ce406d0cfa07e6d4ad6167d0932902
Raja Adeel Ahmed <adeel.ahmed@telenorbank.pk>
2023-04-05 13:30:50 +0500

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by Raja Adeel Ahmed on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
